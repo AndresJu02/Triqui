1b3d86150032f402485d9808ef48d870dde6c90b
AndresJu02 <161164742+AndresJu02@users.noreply.github.com>
2024-02-24 15:45:24 -0500

    Add files via upload

---
+++ EleccionTriqui/EleccionTriqui/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿
+namespace EleccionTriqui
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnMulti = new System.Windows.Forms.Button();
+            this.btnIa = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnMulti
+            // 
+            this.btnMulti.Location = new System.Drawing.Point(41, 264);
+            this.btnMulti.Name = "btnMulti";
+            this.btnMulti.Size = new System.Drawing.Size(100, 42);
+            this.btnMulti.TabIndex = 0;
+            this.btnMulti.Text = "Multiplayer";
+            this.btnMulti.UseVisualStyleBackColor = true;
+            this.btnMulti.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnIa
+            // 
+            this.btnIa.Location = new System.Drawing.Point(57, 258);
+            this.btnIa.Name = "btnIa";
+            this.btnIa.Size = new System.Drawing.Size(90, 48);
+            this.btnIa.TabIndex = 1;
+            this.btnIa.Text = "button2";
+            this.btnIa.UseVisualStyleBackColor = true;
+            this.btnIa.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnMulti);
+            this.groupBox1.Location = new System.Drawing.Point(72, 66);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(204, 331);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnIa);
+            this.groupBox2.Location = new System.Drawing.Point(532, 66);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(205, 317);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnMulti;
+        private System.Windows.Forms.Button btnIa;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+    }
+}
+
---
+++ EleccionTriqui/EleccionTriqui/TriquiMulti.Designer.cs
@@ -0,0 +1,390 @@
+﻿
+namespace Triqui
+{
+    partial class triqui
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.a2 = new System.Windows.Forms.Button();
+            this.a3 = new System.Windows.Forms.Button();
+            this.c1 = new System.Windows.Forms.Button();
+            this.c2 = new System.Windows.Forms.Button();
+            this.a1 = new System.Windows.Forms.Button();
+            this.c3 = new System.Windows.Forms.Button();
+            this.b3 = new System.Windows.Forms.Button();
+            this.b1 = new System.Windows.Forms.Button();
+            this.b2 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblUserPuntos2 = new System.Windows.Forms.Label();
+            this.btnReiniciar = new System.Windows.Forms.Button();
+            this.lblUserPuntos1 = new System.Windows.Forms.Label();
+            this.btnIniciar = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.rbtnUserVO = new System.Windows.Forms.RadioButton();
+            this.rbtnUserVX = new System.Windows.Forms.RadioButton();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.rbtnUserRO = new System.Windows.Forms.RadioButton();
+            this.rbtnUserRX = new System.Windows.Forms.RadioButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(61, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(183, 38);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "T R I Q U I";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel1.Controls.Add(this.a2);
+            this.panel1.Controls.Add(this.a3);
+            this.panel1.Controls.Add(this.c1);
+            this.panel1.Controls.Add(this.c2);
+            this.panel1.Controls.Add(this.a1);
+            this.panel1.Controls.Add(this.c3);
+            this.panel1.Controls.Add(this.b3);
+            this.panel1.Controls.Add(this.b1);
+            this.panel1.Controls.Add(this.b2);
+            this.panel1.Location = new System.Drawing.Point(25, 99);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(284, 284);
+            this.panel1.TabIndex = 12;
+            // 
+            // a2
+            // 
+            this.a2.Location = new System.Drawing.Point(97, 0);
+            this.a2.Name = "a2";
+            this.a2.Size = new System.Drawing.Size(90, 90);
+            this.a2.TabIndex = 7;
+            this.a2.UseVisualStyleBackColor = true;
+            this.a2.Click += new System.EventHandler(this.ButtonsClick);
+            // 
+            // a3
+            // 
+            this.a3.Location = new System.Drawing.Point(193, 0);
+            this.a3.Name = "a3";
+            this.a3.Size = new System.Drawing.Size(90, 90);
+            this.a3.TabIndex = 8;
+            this.a3.UseVisualStyleBackColor = true;
+            this.a3.Click += new System.EventHandler(this.ButtonsClick);
+            // 
+            // c1
+            // 
+            this.c1.Location = new System.Drawing.Point(0, 192);
+            this.c1.Name = "c1";
+            this.c1.Size = new System.Drawing.Size(91, 90);
+            this.c1.TabIndex = 0;
+            this.c1.UseVisualStyleBackColor = true;
+            this.c1.Click += new System.EventHandler(this.ButtonsClick);
+            // 
+            // c2
+            // 
+            this.c2.Location = new System.Drawing.Point(97, 192);
+            this.c2.Name = "c2";
+            this.c2.Size = new System.Drawing.Size(90, 90);
+            this.c2.TabIndex = 1;
+            this.c2.UseVisualStyleBackColor = true;
+            this.c2.Click += new System.EventHandler(this.ButtonsClick);
+            // 
+            // a1
+            // 
+            this.a1.Location = new System.Drawing.Point(0, 0);
+            this.a1.Name = "a1";
+            this.a1.Size = new System.Drawing.Size(91, 90);
+            this.a1.TabIndex = 6;
+            this.a1.UseVisualStyleBackColor = true;
+            this.a1.Click += new System.EventHandler(this.ButtonsClick);
+            // 
+            // c3
+            // 
+            this.c3.Location = new System.Drawing.Point(193, 192);
+            this.c3.Name = "c3";
+            this.c3.Size = new System.Drawing.Size(90, 90);
+            this.c3.TabIndex = 2;
+            this.c3.UseVisualStyleBackColor = true;
+            this.c3.Click += new System.EventHandler(this.ButtonsClick);
+            // 
+            // b3
+            // 
+            this.b3.Location = new System.Drawing.Point(193, 96);
+            this.b3.Name = "b3";
+            this.b3.Size = new System.Drawing.Size(90, 90);
+            this.b3.TabIndex = 5;
+            this.b3.UseVisualStyleBackColor = true;
+            this.b3.Click += new System.EventHandler(this.ButtonsClick);
+            // 
+            // b1
+            // 
+            this.b1.Location = new System.Drawing.Point(0, 96);
+            this.b1.Name = "b1";
+            this.b1.Size = new System.Drawing.Size(91, 90);
+            this.b1.TabIndex = 3;
+            this.b1.UseVisualStyleBackColor = true;
+            this.b1.Click += new System.EventHandler(this.ButtonsClick);
+            // 
+            // b2
+            // 
+            this.b2.Location = new System.Drawing.Point(97, 92);
+            this.b2.Name = "b2";
+            this.b2.Size = new System.Drawing.Size(90, 90);
+            this.b2.TabIndex = 4;
+            this.b2.UseVisualStyleBackColor = true;
+            this.b2.Click += new System.EventHandler(this.ButtonsClick);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox1.Controls.Add(this.lblUserPuntos2);
+            this.groupBox1.Controls.Add(this.btnReiniciar);
+            this.groupBox1.Controls.Add(this.lblUserPuntos1);
+            this.groupBox1.Controls.Add(this.btnIniciar);
+            this.groupBox1.Controls.Add(this.groupBox3);
+            this.groupBox1.Controls.Add(this.groupBox2);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.pictureBox2);
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(341, 25);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(249, 423);
+            this.groupBox1.TabIndex = 13;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "ELIJA QUIÉN EMPIEZA";
+            // 
+            // lblUserPuntos2
+            // 
+            this.lblUserPuntos2.AutoSize = true;
+            this.lblUserPuntos2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUserPuntos2.Location = new System.Drawing.Point(18, 259);
+            this.lblUserPuntos2.Name = "lblUserPuntos2";
+            this.lblUserPuntos2.Size = new System.Drawing.Size(24, 25);
+            this.lblUserPuntos2.TabIndex = 13;
+            this.lblUserPuntos2.Text = "0";
+            // 
+            // btnReiniciar
+            // 
+            this.btnReiniciar.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReiniciar.Location = new System.Drawing.Point(63, 349);
+            this.btnReiniciar.Name = "btnReiniciar";
+            this.btnReiniciar.Size = new System.Drawing.Size(126, 48);
+            this.btnReiniciar.TabIndex = 12;
+            this.btnReiniciar.Text = "REINICIAR";
+            this.btnReiniciar.UseVisualStyleBackColor = true;
+            this.btnReiniciar.Visible = false;
+            this.btnReiniciar.Click += new System.EventHandler(this.btnReiniciar_Click);
+            // 
+            // lblUserPuntos1
+            // 
+            this.lblUserPuntos1.AutoSize = true;
+            this.lblUserPuntos1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblUserPuntos1.Location = new System.Drawing.Point(18, 57);
+            this.lblUserPuntos1.Name = "lblUserPuntos1";
+            this.lblUserPuntos1.Size = new System.Drawing.Size(24, 25);
+            this.lblUserPuntos1.TabIndex = 12;
+            this.lblUserPuntos1.Text = "0";
+            // 
+            // btnIniciar
+            // 
+            this.btnIniciar.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnIniciar.Location = new System.Drawing.Point(82, 349);
+            this.btnIniciar.Name = "btnIniciar";
+            this.btnIniciar.Size = new System.Drawing.Size(85, 48);
+            this.btnIniciar.TabIndex = 10;
+            this.btnIniciar.Text = "INICIAR";
+            this.btnIniciar.UseVisualStyleBackColor = true;
+            this.btnIniciar.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.rbtnUserVO);
+            this.groupBox3.Controls.Add(this.rbtnUserVX);
+            this.groupBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox3.Location = new System.Drawing.Point(76, 323);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(100, 20);
+            this.groupBox3.TabIndex = 9;
+            this.groupBox3.TabStop = false;
+            // 
+            // rbtnUserVO
+            // 
+            this.rbtnUserVO.AutoSize = true;
+            this.rbtnUserVO.Location = new System.Drawing.Point(50, -1);
+            this.rbtnUserVO.Name = "rbtnUserVO";
+            this.rbtnUserVO.Size = new System.Drawing.Size(41, 21);
+            this.rbtnUserVO.TabIndex = 5;
+            this.rbtnUserVO.TabStop = true;
+            this.rbtnUserVO.Text = "O";
+            this.rbtnUserVO.UseVisualStyleBackColor = true;
+            // 
+            // rbtnUserVX
+            // 
+            this.rbtnUserVX.AutoSize = true;
+            this.rbtnUserVX.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rbtnUserVX.Location = new System.Drawing.Point(5, -1);
+            this.rbtnUserVX.Name = "rbtnUserVX";
+            this.rbtnUserVX.Size = new System.Drawing.Size(39, 21);
+            this.rbtnUserVX.TabIndex = 3;
+            this.rbtnUserVX.TabStop = true;
+            this.rbtnUserVX.Text = "X";
+            this.rbtnUserVX.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.BackColor = System.Drawing.Color.Transparent;
+            this.groupBox2.Controls.Add(this.rbtnUserRO);
+            this.groupBox2.Controls.Add(this.rbtnUserRX);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(76, 105);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(100, 27);
+            this.groupBox2.TabIndex = 7;
+            this.groupBox2.TabStop = false;
+            // 
+            // rbtnUserRO
+            // 
+            this.rbtnUserRO.AutoSize = true;
+            this.rbtnUserRO.Location = new System.Drawing.Point(50, 6);
+            this.rbtnUserRO.Name = "rbtnUserRO";
+            this.rbtnUserRO.Size = new System.Drawing.Size(41, 21);
+            this.rbtnUserRO.TabIndex = 5;
+            this.rbtnUserRO.TabStop = true;
+            this.rbtnUserRO.Text = "O";
+            this.rbtnUserRO.UseVisualStyleBackColor = true;
+            // 
+            // rbtnUserRX
+            // 
+            this.rbtnUserRX.AutoSize = true;
+            this.rbtnUserRX.Location = new System.Drawing.Point(5, 6);
+            this.rbtnUserRX.Name = "rbtnUserRX";
+            this.rbtnUserRX.Size = new System.Drawing.Size(39, 21);
+            this.rbtnUserRX.TabIndex = 3;
+            this.rbtnUserRX.TabStop = true;
+            this.rbtnUserRX.Text = "X";
+            this.rbtnUserRX.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(110, 166);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "VS";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::EleccionTriqui.Properties.Resources._5e955184d9d0b;
+            this.pictureBox2.Location = new System.Drawing.Point(76, 230);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(100, 87);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::EleccionTriqui.Properties.Resources._21176;
+            this.pictureBox1.Location = new System.Drawing.Point(76, 21);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 84);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // triqui
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::EleccionTriqui.Properties.Resources._117771667_fondo_de_juego_antiguo_diseño_clásico_de_arcade_retro_con_tubería_y_ladrillo;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(678, 454);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Name = "triqui";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form2";
+            this.Load += new System.EventHandler(this.triqui_Load);
+            this.panel1.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button a2;
+        private System.Windows.Forms.Button a3;
+        private System.Windows.Forms.Button c1;
+        private System.Windows.Forms.Button c2;
+        private System.Windows.Forms.Button a1;
+        private System.Windows.Forms.Button c3;
+        private System.Windows.Forms.Button b3;
+        private System.Windows.Forms.Button b1;
+        private System.Windows.Forms.Button b2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lblUserPuntos2;
+        private System.Windows.Forms.Button btnReiniciar;
+        private System.Windows.Forms.Label lblUserPuntos1;
+        private System.Windows.Forms.Button btnIniciar;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.RadioButton rbtnUserVO;
+        private System.Windows.Forms.RadioButton rbtnUserVX;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton rbtnUserRO;
+        private System.Windows.Forms.RadioButton rbtnUserRX;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
